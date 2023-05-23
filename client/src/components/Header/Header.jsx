2aa2b3c4662d379de35baaf1d8ca10f0b0d15f3a
tarold <ltdecorer@gmail.com>
2023-05-23 22:33:06 +0300

    Add page to nav

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -165,7 +165,9 @@ class Header extends React.Component {
                   />
                   <ul>
                     <li>
-                      <a href='http://www.google.com'>HOW IT WORKS</a>
+                      <Link to='/howItWorks' style={{ textDecoration: 'none' }}>
+                        HOW IT WORKS
+                      </Link>
                     </li>
                     <li>
                       <a href='http://www.google.com'>PRICING</a>
