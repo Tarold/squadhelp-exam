86e267f297f542bb6fe65c46b2e0067a910b27cb
tarold <ltdecorer@gmail.com>
2023-05-23 19:56:25 +0300

    Add page with styles

--- client/src/pages/HowItWorksPage/HowItWorksPage.jsx
+++ client/src/pages/HowItWorksPage/HowItWorksPage.jsx
@@ -1,30 +1,101 @@
 import React from 'react';
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
 
 import styles from './HowItWorksPage.module.sass';
+import CONSTANTS from '../../constants';
 
 const HowItWorksPage = () => {
+  const IMAGES_PATH = `${CONSTANTS.STATIC_IMAGES_PATH}how_it_works/`;
+  //TODO vimeo player
   return (
-    <div>
+    <>
+      <Header />
       <div className={styles.container}>
-        <section className={styles.section}>
-          <h1 className={styles.sectionHeading}>How It Works</h1>
-          <div className={styles.grid}>
-            <div className={styles.gridColumn}>
-              <div className={styles.stepNumber}>1</div>
-              <div className={styles.stepDescription}>Create a Contest</div>
+        <div className={styles.banerSection}>
+          <div className={styles.baner}>
+            <span className={styles.banerNote}>World's #1 Naming Platform</span>
+            <div>
+              <h1>How Does Squadhelp Work?</h1>
+
+              <p>
+                Squadhelp helps you come up with a great name for your business
+                by combining the power of crowdsourcing with sophisticated
+                technology and Agency-level validation services.
+              </p>
             </div>
-            <div className={styles.gridColumn}>
-              <div className={styles.stepNumber}>2</div>
-              <div className={styles.stepDescription}>Receive Ideas</div>
-            </div>
-            <div className={styles.gridColumn}>
-              <div className={styles.stepNumber}>3</div>
-              <div className={styles.stepDescription}>Pick a Winner</div>
+
+            <div className={styles.button}>
+              <a data-fancybox='' href='https://vimeo.com/368584367'>
+                <img className={styles.play} src={`${IMAGES_PATH}play.png`} />
+                Play Video
+              </a>
             </div>
           </div>
-        </section>
+
+          <img className={styles.banerImg} src={`${IMAGES_PATH}phoneguy.png`} />
+        </div>
+        <div className={styles.servicesSection}>
+          <div className={styles.services}>
+            <small className=''>Our Services</small>
+            <h2 className=''>3 Ways To Use Squadhelp</h2>
+            <p className=''>
+              Squadhelp offers 3 ways to get you a perfect name for your
+              business.
+            </p>
+          </div>
+
+          <div className={styles.cardContainer}>
+            <div className={styles.card}>
+              <img src={`${IMAGES_PATH}contest.png`} />
+              <h3 className=''>Launch a Contest</h3>
+              <p>
+                Work with hundreds of creative experts to get custom name
+                suggestions for your business or brand. All names are
+                auto-checked for URL availability.
+              </p>
+              <a className='' href='https://www.squadhelp.com/start-contest'>
+                Launch a Contest
+              </a>
+            </div>
+
+            <div className={styles.card}>
+              <img src={`${IMAGES_PATH}computer.png`} />
+
+              <h3 className=''>Explore Names For Sale</h3>
+              <p>
+                Our branding team has curated thousands of pre-made names that
+                you can purchase instantly. All names include a matching URL and
+                a complimentary Logo Design
+              </p>
+
+              <a
+                className=''
+                href='https://www.squadhelp.com/premium-domains-for-sale/'
+              >
+                Explore Names For Sale
+              </a>
+            </div>
+
+            <div className={styles.card}>
+              <img src={`${IMAGES_PATH}idea.png`} />
+
+              <h3 className=''>Agency-level Managed Contests</h3>
+              <p>
+                Our Managed contests combine the power of crowdsourcing with the
+                rich experience of our branding consultants. Get a complete
+                agency-level experience at a fraction of Agency costs
+              </p>
+
+              <a className='' href='https://www.squadhelp.com/managed-contests'>
+                Learn More
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
