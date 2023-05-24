dcde2b8688611d52e472b77e092cd75910b8603d
tarold <ltdecorer@gmail.com>
2023-05-25 01:28:17 +0300

    Add removeEvent, updateEvent to reducers

--- client/src/store/slices/eventsSlice.js
+++ client/src/store/slices/eventsSlice.js
@@ -10,6 +10,14 @@ const reducers = {
   addEvent: (state, { payload }) => {
     state.events = [...state.events, payload];
   },
+  removeEvent: (state, { payload }) => {
+    state.events = state.events.filter(event => event.id !== payload);
+  },
+  updateEvent: (state, { payload }) => {
+    state.events = state.events.map(event =>
+      event.id === payload.id ? payload : event
+    );
+  },
   clearEvents: state => {
     state.events = [];
   },
@@ -23,6 +31,6 @@ const eventSlice = createSlice({
 
 const { actions, reducer } = eventSlice;
 
-export const { addEvent, getEvents, clearEvents } = actions;
+export const { addEvent, removeEvent, updateEvent, clearEvents } = actions;
 
 export default reducer;
