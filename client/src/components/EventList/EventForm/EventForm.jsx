56bd8d3fafe9715523df956d1d290511b18ea17f
tarold <ltdecorer@gmail.com>
2023-05-25 00:04:11 +0300

    Add DatePicker to Form and replace validationSchem

--- client/src/components/EventList/EventForm/EventForm.jsx
+++ client/src/components/EventList/EventForm/EventForm.jsx
@@ -1,61 +1,22 @@
 import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-
-// CSS Modules, react-datepicker-cssmodules.css//
-import 'react-datepicker/dist/react-datepicker-cssmodules.css';
-import { setHours, setMinutes } from 'date-fns';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as yup from 'yup';
-
+import DatePickerComponent from '../../InputComponents/DatePicker/DatePicker';
+import { v4 as uuid } from 'uuid';
+import Schems from '../../../utils/validators/validationSchems';
 import styles from './EventForm.module.sass';
-//TODO mix man Date picker. Exept time from date. Return values from form like then. Add key
+
 const EventForm = ({ onSubmit }) => {
+  const validationSchema = Schems.EventSchema;
   const initialValues = {
     eventName: '',
-    testDayTime: '',
-    testDayTimeTnen: '',
-    eventTime: '',
-    notificationTime: '',
+    eventDate: '',
+    notificationDate: '',
   };
 
-  const validationSchema = yup.object().shape({
-    eventName: yup.string().required('Event Name is required'),
-    eventTime: yup
-      .date()
-      .min(new Date(), 'Date must be after current date')
-      .required('Event Date is required'),
-    notificationTime: yup
-      .date()
-      .required('Date is required')
-      .min(new Date(), 'Date must be after current date')
-      .max(yup.ref('eventTime'), 'Date must be before or equal to Event Date'),
-  });
+  const [date, setDate] = useState(initialValues.eventDate);
 
-  const [startDate, setStartDate] = useState(
-    setHours(setMinutes(new Date(), 0), 9)
-  );
-  const [startDateThen, setStartDateThen] = useState(
-    setHours(setMinutes(new Date(), 0), 9)
-  );
-  const filterPassedTime = time => {
-    const currentDate = new Date();
-    const selectedDate = new Date(time);
-
-    return currentDate.getTime() < selectedDate.getTime();
-  };
-  const filterPassedTimeThen = (time, after) => {
-    const currentDate = new Date();
-    const selectedDate = new Date(time);
-    const selectedDateAfter = new Date(after);
-    console.log(time, after);
-    return (
-      currentDate.getTime() < selectedDate.getTime() &&
-      selectedDate.getTime() <= selectedDateAfter
-    );
-  };
   const handleSubmit = (values, { resetForm }) => {
-    values.testDayTime = startDate;
+    values.id = uuid();
     onSubmit(values);
     resetForm(initialValues);
   };
@@ -78,42 +39,31 @@ const EventForm = ({ onSubmit }) => {
         </div>
         <div className={styles.field}>
           <label htmlFor='eventDate'>Event Date</label>
-          <Field type='datetime-local' id='eventTime' name='eventTime'></Field>
+          <DatePickerComponent
+            id='eventDate'
+            name='eventDate'
+            setChange={setDate}
+          ></DatePickerComponent>
           <ErrorMessage
-            name='eventTime'
+            name='eventDate'
             component='div'
             className={styles.error}
           />
         </div>
         <div className={styles.field}>
-          <label htmlFor='notificationTime'>Notification Date</label>
-          <Field
-            type='datetime-local'
-            id='notificationTime'
-            name='notificationTime'
-          ></Field>
+          <label htmlFor='notificationDate'>Notification Date</label>
+          <DatePickerComponent
+            id='notificationDate'
+            name='notificationDate'
+            after={date}
+          ></DatePickerComponent>
           <ErrorMessage
-            name='notificationTime'
+            name='notificationDate'
             component='div'
             className={styles.error}
           />
         </div>
-        <DatePicker
-          name='testDayTime'
-          selected={startDate}
-          onChange={date => setStartDate(date)}
-          showTimeSelect
-          filterTime={filterPassedTime}
-          dateFormat='MMMM d, yyyy h:mm aa'
-        />
-        <DatePicker
-          name='testDayTimeThen'
-          selected={startDateThen}
-          onChange={date => setStartDateThen(date)}
-          showTimeSelect
-          filterTime={date => filterPassedTimeThen(date, startDate)}
-          dateFormat='MMMM d, yyyy h:mm aa'
-        />
+
         <button type='submit'>Create Timer</button>
       </Form>
     </Formik>
