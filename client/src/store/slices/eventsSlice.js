4836c0f0249d3f45fb598e205bf4777700124cc0
tarold <ltdecorer@gmail.com>
2023-05-24 20:47:40 +0300

    Add eventsReducer, eventSlice

---
+++ client/src/store/slices/eventsSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const EVENT_SLICE_NAME = 'event';
+
+const initialState = {
+  events: [],
+};
+
+const reducers = {
+  addEvent: (state, { payload }) => {
+    state.events = [...state.events, payload];
+  },
+  clearEvents: state => {
+    state.events = [];
+  },
+};
+
+const eventSlice = createSlice({
+  name: `${EVENT_SLICE_NAME}`,
+  initialState,
+  reducers,
+});
+
+const { actions, reducer } = eventSlice;
+
+export const { addEvent, getEvents, clearEvents } = actions;
+
+export default reducer;
