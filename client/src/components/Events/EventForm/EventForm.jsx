9b1075f2d3a55a7664279b3aeb3f6f5e76a43bf2
tarold <ltdecorer@gmail.com>
2023-05-24 17:10:43 +0300

    Add EventForm

--- client/src/components/Events/EventForm/EventForm.jsx
+++ client/src/components/Events/EventForm/EventForm.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+
+import styles from './EventForm.module.sass';
 
 const EventForm = ({ onSubmit }) => {
   const initialValues = {
     eventName: '',
-    eventDate: '',
-    eventTime: '10:00',
+    eventTime: '',
     notificationTime: '',
   };
+
+  const validationSchema = yup.object().shape({
+    eventName: yup.string().required('Event Name is required'),
+    eventTime: yup.date().required('Event Date is required'),
+    notificationTime: yup
+      .date()
+      .required('Notification Date is required')
+      .min(new Date(), 'Notification Date must be after current date')
+      .max(
+        yup.ref('eventTime'),
+        'Notification Date must be before or equal to Event Date'
+      ),
+  });
 
   const handleSubmit = (values, { resetForm }) => {
     onSubmit(values);
@@ -15,20 +30,43 @@ const EventForm = ({ onSubmit }) => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-      <Form>
-        <label htmlFor='eventName'>Event Name</label>
-        <Field type='text' id='eventName' name='eventName' />
-
-        <label htmlFor='eventDate'>Event Date</label>
-        <Field type='date' id='eventDate' name='eventDate' />
-
-        <label htmlFor='eventTime'>Event Time</label>
-        <Field type='time' id='eventTime' name='eventTime' />
-
-        <label htmlFor='notificationTime'>Notification Time (in seconds)</label>
-        <Field type='number' id='notificationTime' name='notificationTime' />
-
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema}
+    >
+      <Form className={styles.eventForm}>
+        <div className={styles.field}>
+          <label htmlFor='eventName'>Event Name</label>
+          <Field type='text' id='eventName' name='eventName'></Field>
+          <ErrorMessage
+            name='eventName'
+            component='div'
+            className={styles.error}
+          />
+        </div>
+        <div className={styles.field}>
+          <label htmlFor='eventDate'>Event Date</label>
+          <Field type='datetime-local' id='eventTime' name='eventTime'></Field>
+          <ErrorMessage
+            name='eventTime'
+            component='div'
+            className={styles.error}
+          />
+        </div>
+        <div className={styles.field}>
+          <label htmlFor='notificationTime'>Notification Date</label>
+          <Field
+            type='datetime-local'
+            id='notificationTime'
+            name='notificationTime'
+          ></Field>
+          <ErrorMessage
+            name='notificationTime'
+            component='div'
+            className={styles.error}
+          />
+        </div>
         <button type='submit'>Create Timer</button>
       </Form>
     </Formik>
