92403276c6dcf02ff676e71ea69aab13d03cf433
tarold <ltdecorer@gmail.com>
2023-05-25 07:58:28 +0300

    Refactor code & add notification & add interval

--- client/src/components/EventList/Event/Event.jsx
+++ client/src/components/EventList/Event/Event.jsx
@@ -1,44 +1,58 @@
+import { useEffect, useState } from 'react';
 import styles from './Event.module.sass';
+import formatDuration from 'date-fns/formatDuration';
+import intervalToDuration from 'date-fns/intervalToDuration';
+import differenceInMilliseconds from 'date-fns/differenceInMilliseconds';
 
-function getDateBefore (now, length) {
-  const millisecondsPerHour = 1000 * 60 * 60;
-  const millisecondsPerDay = millisecondsPerHour * 24;
+import classNames from 'classnames';
 
-  const dateMilliseconds = length - now;
-  const days = Math.round(dateMilliseconds / millisecondsPerDay);
+function getDateBefore (date) {
+  const start = new Date();
+  const end = new Date(date);
 
-  if (days === 0) {
-    return 'today';
+  if (start >= end) {
+    return 'now';
   }
-  const hoursMilliseconds = dateMilliseconds - days * millisecondsPerDay;
-  const hours = Math.round(hoursMilliseconds / millisecondsPerHour);
-  if (hours === 0) {
-    return `${days} days left`;
+
+  const duration = intervalToDuration({ start: start, end: end });
+  const formattedDuration = formatDuration(duration, {
+    format: ['days', 'hours'],
+  });
+
+  if (!formattedDuration) {
+    return (
+      'Soon! ' +
+      formatDuration(duration, {
+        format: ['minutes'],
+      })
+    );
   }
-  return `${days} days ${hours} hours left`;
+
+  return formattedDuration;
 }
 
 function progressBarHelper (eventDate, notificationDate, startDate) {
   const getBetweenTime = (startDate, eventDate) => {
-    const start = new Date(startDate).getTime();
-    const event = new Date(eventDate).getTime();
-    return event - start;
+    return differenceInMilliseconds(new Date(startDate), new Date(eventDate));
   };
 
   const length = getBetweenTime(startDate, eventDate);
   const triger = getBetweenTime(startDate, notificationDate);
   const now = getBetweenTime(startDate, new Date());
 
-  const countDays = getDateBefore(now, length);
-
   const procentTriger = Math.round((triger / length) * 100);
   const procentNow = Math.round((now / length) * 100);
+
+  let green = 'green';
+  if (procentNow >= procentTriger) {
+    green = 'lightgreen';
+  }
   const gradientStyle = {
-    background: `linear-gradient(to right, green 0%, green ${procentNow}%, white ${procentNow}%, white ${procentTriger}%, yellow ${procentTriger}%, yellow 100%)`,
+    background: `linear-gradient(to right, ${green} 0%, ${green} ${procentNow}%, white ${procentNow}%, white ${procentTriger}%, yellow ${procentTriger}%, yellow 100%)`,
   };
-  return [gradientStyle, countDays];
+  return gradientStyle;
 }
-//TODO update per minute
+
 const Event = ({
   eventName,
   eventDate,
@@ -46,19 +60,85 @@ const Event = ({
   startDate,
   del,
   enableEdit,
+  notification,
+  setNotification,
+  eventsCount,
 }) => {
-  const [gradientStyle, countDays] = progressBarHelper(
-    eventDate,
-    notificationDate,
-    startDate
-  );
+  const [countDays, setCountDays] = useState();
+  const [progresStyle, setProgresStyle] = useState();
+  const [eventStyle, setEventStyle] = useState();
+  const [isFinish, setIsFinish] = useState(false);
 
+  const updateEvent = () => {
+    setCountDays(getDateBefore(eventDate));
+    setProgresStyle(progressBarHelper(eventDate, notificationDate, startDate));
+    setEventStyle(
+      classNames(
+        [styles.event],
+        { [styles.finish]: new Date() >= eventDate },
+        {
+          [styles.notification]: notification,
+        }
+      )
+    );
+  };
+
+  useEffect(() => {
+    updateEvent();
+    const millisecondsToFinish = differenceInMilliseconds(
+      new Date(eventDate),
+      new Date()
+    );
+    const millisecondsToNotification = differenceInMilliseconds(
+      new Date(notificationDate),
+      new Date()
+    );
+    const interval = setInterval(() => {
+      updateEvent();
+    }, new Date(eventDate).getTime * 0.01);
+    const timeoutNotification =
+      millisecondsToNotification > 0
+        ? setTimeout(() => {
+            setNotification('task is soon');
+          }, millisecondsToNotification)
+        : '';
+    const timeoutFinish =
+      millisecondsToFinish > 0
+        ? setTimeout(() => {
+            updateEvent();
+            setIsFinish(true);
+            setNotification('timer finish');
+            clearInterval(interval);
+          }, millisecondsToFinish)
+        : setIsFinish(true);
+
+    return () => {
+      clearInterval(interval);
+      clearTimeout(timeoutFinish);
+      clearTimeout(timeoutNotification);
+    };
+  }, []);
   return (
-    <li className={styles.event} style={gradientStyle}>
+    <li className={eventStyle} style={progresStyle}>
       <span className={styles.name}>{eventName}</span>
-      <span className={styles.countDays}>{countDays}</span>
+
+      {isFinish ? (
+        <span className={styles.name}>eventsCount:{eventsCount}</span>
+      ) : (
+        <>
+          <span className={styles.countDays}>{countDays}</span>
+          <button onClick={enableEdit}>Edit</button>
+        </>
+      )}
+      {notification ? (
+        <div>
+          Notification!!! {notification}
+          <button onClick={() => setNotification('')}>x</button>
+        </div>
+      ) : (
+        ''
+      )}
       <button onClick={del}>Delete</button>
-      <button onClick={enableEdit}>Edit</button>
     </li>
   );
 };
