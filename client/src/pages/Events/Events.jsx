79049ab591a21243ae6dbf86cc4823c25ce78ebb
tarold <ltdecorer@gmail.com>
2023-05-25 12:28:32 +0300

    Add styles and title to page

--- client/src/pages/Events/Events.jsx
+++ client/src/pages/Events/Events.jsx
@@ -1,11 +1,12 @@
 import EventList from '../../components/EventList/EventList';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
-
+import styles from './Events.module.sass';
 const Events = () => {
   return (
     <>
       <Header />
+      <h1 className={styles.title}>Your Events</h1>
       <EventList />
       <Footer />
     </>
