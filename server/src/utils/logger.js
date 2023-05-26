3213bcf34f77a7546edcd3ad7fec1b5f8b19473d
tarold <ltdecorer@gmail.com>
2023-05-26 10:42:28 +0300

    Add daily-rotate-file & data transform

--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -1,8 +1,13 @@
 const winston = require('winston');
+const fs = require('fs');
 const { format, transports } = require('winston');
+require('winston-daily-rotate-file');
 const { combine, printf, errors } = format;
+const CONSTANTS = require('../constants');
+const path = require('path');
+const readline = require('readline');
 
-const myFormat = printf(({ message, code, stack }) => {
+const logFormat = printf(({ message, code, stack }) => {
   stack = stack.split('\n    ').slice(1);
   return JSON.stringify({
     message: message,
@@ -12,11 +17,43 @@ const myFormat = printf(({ message, code, stack }) => {
   });
 });
 
+const dailyRotateFormat = data => {
+  const { message, code, time } = JSON.parse(data);
+  return JSON.stringify({
+    message,
+    code,
+    time,
+  });
+};
+
+const dailyRotateTransport = new transports.DailyRotateFile({
+  filename: `%DATE%.log`,
+  dirname: CONSTANTS.LOG_DIR,
+  datePattern: `YYYY-MM-DD`,
+  maxSize: `20m`,
+  maxFiles: `14d`,
+});
+
+dailyRotateTransport.on('rotate', function (oldFilename, newFilename) {
+  const inputFile = path.join(CONSTANTS.LOG_DIR, CONSTANTS.LOG_FILE);
+
+  const readFile = readline.createInterface({
+    input: fs.createReadStream(inputFile),
+    output: fs.createWriteStream(oldFilename),
+    terminal: false,
+  });
+
+  readFile.on('line', dailyRotateFormat).on('close', () => {
+    fs.truncate(inputFile, 0, function () {});
+  });
+});
+
 const logConfiguration = {
   transports: [
     new transports.File({
-      format: combine(errors({ stack: true }), myFormat),
-      filename: 'logs/example.log',
+      format: combine(errors({ stack: true }), logFormat),
+      filename: CONSTANTS.LOG_FILE,
+      dirname: CONSTANTS.LOG_DIR,
     }),
   ],
 };
