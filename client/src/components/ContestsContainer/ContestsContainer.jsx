2183eafdfe2eb98853d7654896620a484cb6b142
tarold <ltdecorer@gmail.com>
2023-05-29 23:48:57 +0300

    Nothing not found to Nothing found

--- client/src/components/ContestsContainer/ContestsContainer.jsx
+++ client/src/components/ContestsContainer/ContestsContainer.jsx
@@ -26,7 +26,7 @@ class ContestsContainer extends React.Component {
   render () {
     const { isFetching } = this.props;
     if (!isFetching && this.props.children.length === 0) {
-      return <div className={styles.notFound}>Nothing not found</div>;
+      return <div className={styles.notFound}>Nothing found</div>;
     }
     return (
       <div>
