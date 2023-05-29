bcd525946dd7411b061a09691963f979bb88adc9
tarold <ltdecorer@gmail.com>
2023-05-29 23:49:24 +0300

    Add ModeratorDashboard

--- client/src/pages/OffersPage/OffersPage.jsx
+++ client/src/pages/OffersPage/OffersPage.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import CONSTANTS from '../../constants';
-import CustomerDashboard from '../../components/CustomerDashboard/CustomerDashboard';
-import CreatorDashboard from '../../components/CreatorDashboard/CreatorDashboard';
 import Header from '../../components/Header/Header';
+import ModeratorDashboard from '../../components/ModeratorDashboard/ModeratorDashboard';
 
 const Dashboard = props => {
-  const { role, history } = props;
+  const { history } = props;
   return (
     <div>
       <Header />
-      {role === CONSTANTS.CUSTOMER ? (
-        <CustomerDashboard history={history} match={props.match} />
-      ) : (
-        <CreatorDashboard history={history} match={props.match} />
-      )}
+      <ModeratorDashboard history={history} match={props.match} />
     </div>
   );
 };
