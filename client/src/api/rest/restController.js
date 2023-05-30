894e11b787719246e7d9efa086da59f7718709ff
tarold <ltdecorer@gmail.com>
2023-05-30 09:58:05 +0300

    Change path in restController

--- client/src/api/rest/restController.js
+++ client/src/api/rest/restController.js
@@ -4,11 +4,12 @@ import http from '../interceptor';
 export const getUser = () => http.get('user');
 export const registerRequest = data => http.post('user/registration', data);
 export const loginRequest = data => http.post('user/login', data);
+export const changeMark = data => http.patch('user/changeMark', data);
+export const cashOut = data => http.post('user/cashout', data);
+export const updateUser = data => http.patch('user', data);
 
 export const payMent = data => http.post('contests', data.formData);
 export const dataForContest = data => http.post('contests/data', data);
-export const setNewOffer = data => http.post('contests/offers', data);
-export const setOfferStatus = data => http.patch('contests/offers', data);
 export const downloadContestFile = data =>
   http.get(`contests/file/${data.fileName}`);
 export const getCustomersContests = data =>
@@ -21,15 +22,16 @@ export const updateContest = ({ contestId, data }) => {
 export const getContestById = ({ contestId }) =>
   http.get(`contests/${contestId}`);
 
+export const setNewOffer = data => http.post('offers', data);
+export const setOfferStatus = data => http.patch('offers', data);
+export const getNoVerifyingOffers = data =>
+  http.get(`offers?${queryString.stringify(data)}`);
+
 export const newMessage = data => http.post('chat', data);
 export const getPreviewChat = () => http.get('chat');
 export const getDialog = data => http.get(`chat/${data}`);
 export const changeChatBlock = data => http.post('chat/block', data);
 export const changeChatFavorite = data => http.post('chat/fav', data);
-
-export const changeMark = data => http.patch('user/changeMark', data);
-export const cashOut = data => http.post('user/cashout', data);
-export const updateUser = data => http.patch('user', data);
 
 export const getCatalogList = data => http.get('chat/catalog', data);
 export const createCatalog = data => http.post('chat/catalog', data);
