afd79ee92d87170cdea5923ce4019c0343dbad74
tarold <ltdecorer@gmail.com>
2023-05-31 10:18:20 +0300

    Add test send email

--- server/src/controllers/emailController.js
+++ server/src/controllers/emailController.js
@@ -1,30 +1,56 @@
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 const CONSTANTS = require('../constants');
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'umenaotvet',
-    pass: CONSTANTS.CONTACT_EMAIL_PASSWORD,
-  },
-});
+const address = 'http://localhost:3000/contest/';
 
+async function getTestTransporter () {
+  const testAccount = await nodemailer.createTestAccount();
+
+  const testTransporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false,
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
+  });
+
+  return testTransporter;
+}
+
+async function getTransporter () {
+  const transporter = nodemailer.createTransport({
+    host: CONSTANTS.CONTACT_EMAIL_HOST,
+    port: 587,
+    secure: false,
+    auth: {
+      user: CONSTANTS.CONTACT_EMAIL,
+      pass: CONSTANTS.CONTACT_EMAIL_PASSWORD,
+    },
+  });
+  return transporter;
+}
 const mailOptions = ({ email, offer }) => {
-  console.log('offer :>> ', email, offer);
   return {
     from: CONSTANTS.CONTACT_EMAIL,
-    to: 'ltdecorer@gmail.com',
+    to: email,
     subject: 'Moderator has reviewed your offer',
-    text: `Dear Creator,
-
+    text: `
+      Dear Creator,
+    
     We hope this email finds you well. We wanted to inform you that a moderator has reviewed your offer.
     
-    Here are the details of your offer:
+      Offer Details:
     
-    Text: '7'
+    Offer ID: ${offer.id}
+    Text: ${offer.text}
     Approval Status: ${offer.isApproved}
-
-    squadhelp.com/contests/${1}
+    
+    We appreciate your contribution and value your participation. Your approved offer will now be processed accordingly.
+    
+    To access the Squadhelp contest page, click on the following link: 
+      ${address}${offer.contestId}
     
     If you have any further questions or concerns, please don't hesitate to reach out to us. We are here to assist you.
     
@@ -33,15 +59,64 @@ const mailOptions = ({ email, offer }) => {
     Best regards,
     Squadhelp
     `,
+    html: `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Moderator Approval</title>
+    </head>
+    <body>
+      <h2>Dear Creator,</h2>
+      
+      <p>We hope this email finds you well. We wanted to inform you that a moderator has reviewed your offer.</p>
+      
+      <h3>Offer Details:</h3>
+      <ul>
+        <li><strong>Offer ID:</strong> ${offer.id}</li>
+        <li><strong>Text:</strong> ${offer.userId}</li>
+        <li><strong>Approval Status:</strong> ${offer.isApproved}</li>
+      </ul>
+      
+      <p>We appreciate your contribution and value your participation. Your approved offer will now be processed accordingly.</p>
+      
+      <!-- Button with link to Squadhelp contest page -->
+      <p>
+        <a href="${address}${offer.contestId}" style="display: inline-block; background-color: #3f51b5; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 4px;">
+          Go to Contest Page
+        </a>
+      </p>
+      
+      <p>If the button above doesn't work, you can manually copy and paste the following link into your web browser:</p>
+      
+      <!-- Link in case the button doesn't work -->
+      <p><a href="${address}${offer.contestId}">${address}${offer.contestId}</a></p>
+      
+      <p>If you have any further questions or concerns, please don't hesitate to reach out to us. We are here to assist you.</p>
+      
+      <p>Thank you for your continued support.</p>
+      
+      <p>Best regards,<br>
+      Squadhelp<br>
+      ${CONSTANTS.CONTACT_EMAIL}</p>
+    </body>
+    </html>
+    `,
   };
 };
 
-module.exports.sendOfferMessage = props => {
-  transporter.sendMail(mailOptions(props), function (error, info) {
-    if (error) {
-      console.log('object :>> ', Error(error));
-    } else {
-      return true;
+module.exports.sendOfferMessage = async props => {
+  let transporter;
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    transporter = await getTestTransporter();
+  } else {
+    transporter = await getTransporter();
+  }
+  try {
+    const info = await transporter.sendMail(mailOptions(props));
+
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+      console.log('Email :>> ', nodemailer.getTestMessageUrl(info));
     }
-  });
+  } catch (err) {
+    next(err);
+  }
 };
