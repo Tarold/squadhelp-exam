64d9f48a1c2ec25150fecc6f3531378db3ba95fa
tarold <ltdecorer@gmail.com>
2023-06-03 13:44:51 +0300

    Add HelpListNav

--- client/src/components/HelpList/HelpList.jsx
+++ client/src/components/HelpList/HelpList.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import HelpSection from './HelpSection/HelpSection';
+import HelpListNav from './HelpListNav/HelpListNav';
 
 const HelpList = ({ data }) => {
   const mapList = data => <HelpSection data={data} />;
-  return <div>{data.map(mapList)}</div>;
+
+  return (
+    <div>
+      <HelpListNav data={data} />
+      <div>{data.map(mapList)}</div>
+    </div>
+  );
 };
 export default HelpList;
--- client/src/components/HelpList/HelpListNav/HelpListNav.jsx
+++ client/src/components/HelpList/HelpListNav/HelpListNav.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
-const HelpListNav = () => {
-  return <div>Header</div>;
+const HelpListNav = ({ data }) => {
+  const headerList = data.map(section => (
+    <div>
+      <a href={'#' + section.id}>
+        <p>{section.title}</p>
+      </a>
+    </div>
+  ));
+  return <div>{headerList}</div>;
 };
 export default HelpListNav;
